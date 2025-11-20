1c7cd32ae742dad7a58ba9ae4f261370864511d0
nowah00 <hawon2k@gmail.com>
2025-11-20 12:39:29 +0900

    First commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from 'vue';
+import AppPropsLiteral from './components/props/AppPropsLiteral.vue';
+const view = ref('');
+</script>
+
+<template>
+  <div class="container">
+    <TheHeading />
+    <div class="menu">
+      <button :class="{ active: view === 'literal' }" @click="view = 'literal'">
+        Props Literal
+      </button>
+      <button :class="{ active: view === 'object' }" @click="view = 'object'">
+        Props Object
+      </button>
+      <button
+        :class="{ active: view === 'template' }"
+        @click="view = 'template'"
+      >
+        Component Events (template)
+      </button>
+      <button :class="{ active: view === 'script' }" @click="view = 'script'">
+        Component Events (script setup)
+      </button>
+    </div>
+    <AppPropsLiteral v-if="view === 'literal'" view-title="좋아하는 과일은?" />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+}
+.menu {
+  margin: 0.3rem 0;
+}
+button {
+  margin: 0.2rem;
+  padding: 0.5rem;
+}
+.active {
+  background-color: steelblue;
+  color: aliceblue;
+  border: 0;
+  border-radius: 5px;
+}
+</style>
